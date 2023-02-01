8ca8b79471d68adc87dbc27644a6192dadd1a609
Harshal168 <122151720+Harshal168@users.noreply.github.com>
2023-02-01 22:27:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var x =document.getElementById("login")
+var y =document.getElementById("register")
+var z =document.getElementById("toggle")
+
+function register(){
+    x.style.left = "-400px"
+    y.style.left = "-300px"
+    z.style.left = "100px"
+}
+
+function login(){
+    x.style.left = "0px"
+    y.style.left = "300px"
+    z.style.left = "0px"
+}
